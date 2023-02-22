debf5fba7a65fea5fa84f98597ebc20797b94405
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-02-23 00:37:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from functions import *
 
-# print(euclideanDistance(np.array([3,0,0]), np.array([0,4,1])))
-#Result = 1.41421
 S = np.array([[2,3],[12,30],[40,50],[5,1],[12,10],[3,4]])
 S2 = np.array([[1,3,0,1],[12,30,0,1],[40,50,0,1],[5,1,0,0],[12,10,0,0],[3,4,0,1]])
+#Result = 1.41421
 
 lower_bound = 0
 upper_bound = 5
@@ -21,8 +20,8 @@ for i in range(n):
     # print()
     # List_Points = np.append(List_Points, np.array([[x1,x2,x3]]), axis=0)
     List_Points = np.append(List_Points, np.random.randint(lower_bound, upper_bound, size=(1, 3)), axis=0)
+
 # List_Points = np.append(List_Points, np.array([[7, 8, 9]]), axis=0)
-print()
 print()
 print(List_Points)
 print(DivNCon(sortList(List_Points)))
