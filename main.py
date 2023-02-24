5d17b0f8f829d12b79197789de58204197f5830e
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-02-25 00:14:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,20 +8,24 @@ lower_bound = 0
 upper_bound = 5
 dimension = 3
 
-print("Masukan banyak titik")
-n = int(input("n = "))
-List_Points = np.empty((0,dimension), int)
-print()
-for i in range(n):
-    # print("Masukan titik ke-", i+1, ": ")
-    # x1 = int(input())
-    # x2 = int(input())
-    # x3 = int(input())
-    # print()
-    # List_Points = np.append(List_Points, np.array([[x1,x2,x3]]), axis=0)
-    List_Points = np.append(List_Points, np.random.randint(lower_bound, upper_bound, size=(1, 3)), axis=0)
+# Stest = np.array([[1,1,0],[3,0,1],[7,8,9]])
+Stest = np.array([[4,3,1],[3,1,0],[3,3,4],[4,3,0]])
+# print(EuclideanDistance(Stest[0], Stest[1]))
+
+# print("Masukan banyak titik")
+# n = int(input("n = "))
+# List_Points = np.empty((0,dimension), int)
+# print()
+# for i in range(n):
+#     # print("Masukan titik ke-", i+1, ": ")
+#     # x1 = int(input())
+#     # x2 = int(input())
+#     # x3 = int(input())
+#     # print()
+#     # List_Points = np.append(List_Points, np.array([[x1,x2,x3]]), axis=0)
+#     List_Points = np.append(List_Points, np.random.randint(lower_bound, upper_bound, size=(1, 3)), axis=0)
 
 # List_Points = np.append(List_Points, np.array([[7, 8, 9]]), axis=0)
-print()
-print(List_Points)
-print(DivNCon(sortList(List_Points)))
+print(Stest)
+# print(List_Points)
+print(DivNCon(sortList(Stest)))
