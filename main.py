d6b3399b5d8d973e0bab40dcb5ae3df64ca24364
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-02-25 00:46:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ S2 = np.array([[1,3,0,1],[12,30,0,1],[40,50,0,1],[5,1,0,0],[12,10,0,0],[3,4,0,1]
 #Result = 1.41421
 
 lower_bound = 0
-upper_bound = 5
+upper_bound = 20
 dimension = 3
 
 # Stest = np.array([[1,1,0],[3,0,1],[7,8,9]])
 Stest = np.array([[4,3,1],[3,1,0],[3,3,4],[4,3,0]])
+# Stest = np.array([[1,0,0],[2,0,1],[2,0,2]])
 # print(EuclideanDistance(Stest[0], Stest[1]))
 
 print("Masukan banyak titik")
@@ -29,4 +30,7 @@ for i in range(n):
 # print(Stest)
 print(List_Points)
 # print(DivNCon(sortList(Stest)))
-print(DivNCon(sortList(List_Points)))
+hasil = DivNCon(sortList(List_Points))
+print("titik 1 = ", hasil[1])
+print("titik 2 = ", hasil[2])
+print(hasil[0])
