f0d0f7f4788a289ae124255cc48afccd62f7abad
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-02-26 11:16:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ Stest3 = np.array([[79,40],
  [87 ,96],
  [67 ,73],
  [71 ,58]])
+Stest4 = np.array([[51, 32],[51, 32],[78, 20],[57, 47], [4, 80]])
+Stest5 = np.array([[51, 32],[51, 32],[78, 20], [4, 80]])
 # Stest = np.array([[1,0,0],[2,0,1],[2,0,2]])
 # print(EuclideanDistance(Stest[0], Stest[1]))
 
@@ -39,7 +41,7 @@ print(List_Points)
 
 List_PointsBruteForce = List_Points.copy()
 hasil = DivNCon(sortList(List_Points))
-hasilB = BruteForce(sortList(List_PointsBruteForce))  
+hasilB = BruteForce(sortList(List_PointsBruteForce))
 # hasil = DivNCon(sortList(Stest3))
 # hasilB = BruteForce(sortList(Stest3))  
 print("titik 1 = ", hasil[1])
@@ -51,3 +53,9 @@ print("titik 1 Brute = ", hasilB[1])
 print("titik 2 Brute = ", hasilB[2])
 print(hasilB[0])
 # from viz import *
+
+print()
+if (hasil[0] == hasilB[0]):
+    print("Hasil sama")
+else:
+    print("SALAHH")
