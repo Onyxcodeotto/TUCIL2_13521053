88728ac034ef724de57891258a836f003ef8976c
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-02-26 11:28:59 +0700

    ada masalah
    Stest6

--- main.py
+++ main.py
@@ -18,6 +18,96 @@ Stest3 = np.array([[79,40],
  [71 ,58]])
 Stest4 = np.array([[51, 32],[51, 32],[78, 20],[57, 47], [4, 80]])
 Stest5 = np.array([[51, 32],[51, 32],[78, 20], [4, 80]])
+Stest6 = np.array([[42, 91],
+ [95, 17],
+ [17, 96],
+ [87, 37],
+ [74,  8],
+ [15, 41],
+ [22,  9],
+ [96, 25],
+ [ 1, 48],
+ [36, 25],
+ [29, 49],
+ [60, 69],
+ [ 9,  4],
+ [16, 93],
+ [82, 57],
+ [ 0, 25],
+ [55, 73],
+ [80,  1],
+ [ 6, 50],
+ [20, 60],
+ [49, 19],
+ [ 9, 45],
+ [40, 54],
+ [98, 32],
+ [48, 44],
+ [93, 95],
+ [25, 30],
+ [59, 38],
+ [ 2, 44],
+ [38, 23],
+ [49, 76],
+ [24, 91],
+ [36, 47],
+ [31, 77],
+ [49,  4],
+ [67, 74],
+ [61,  7],
+ [ 7,  8],
+ [78, 50],
+ [52, 26],
+ [92, 58],
+ [58, 94],
+ [75, 81],
+ [55,  4],
+ [29, 66],
+ [97, 98],
+ [57, 69],
+ [37, 83],
+ [21, 69],
+ [10, 65],
+ [63, 50],
+ [69, 73],
+ [29,  5],
+ [84, 43],
+ [40, 14],
+ [55, 22],
+ [12, 56],
+ [89, 73],
+ [68,  3],
+ [54, 26],
+ [13, 15],
+ [ 3, 88],
+ [83, 82],
+ [82, 83],
+ [20, 94],
+ [71, 90],
+ [ 0, 70],
+ [41, 95],
+ [13, 51],
+ [ 9, 55],
+ [98, 38],
+ [58,  8],
+ [25, 75],
+ [29, 65],
+ [26,  2],
+ [95, 22],
+ [30, 41],
+ [66, 79],
+ [25, 91],
+ [97, 52],
+ [69,  7],
+ [48, 97],
+ [95,  3],
+ [49, 96],
+ [85, 74],
+ [64, 76],
+ [13, 63],
+ [ 0, 68],
+ [60, 17],
+ [23, 35]])
 # Stest = np.array([[1,0,0],[2,0,1],[2,0,2]])
 # print(EuclideanDistance(Stest[0], Stest[1]))
 
@@ -40,10 +130,10 @@ print(List_Points)
 # print(DivNCon(sortList(Stest)))
 
 List_PointsBruteForce = List_Points.copy()
-hasil = DivNCon(sortList(List_Points))
-hasilB = BruteForce(sortList(List_PointsBruteForce))
-# hasil = DivNCon(sortList(Stest3))
-# hasilB = BruteForce(sortList(Stest3))  
+# hasil = DivNCon(sortList(List_Points))
+# hasilB = BruteForce(sortList(List_PointsBruteForce))
+hasil = DivNCon(sortList(Stest6))
+hasilB = BruteForce(sortList(Stest6))  
 print("titik 1 = ", hasil[1])
 print("titik 2 = ", hasil[2])
 print(hasil[0])
